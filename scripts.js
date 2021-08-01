78e9517fb6bd4ec5d4108b799c02741b516f6fbe
mark nuchiri macharia <marknuchirimacharia@marks-MacBook-Air.local>
2021-08-01 19:11:13 +0300

    added getting values for pizza

--- scripts.js
+++ scripts.js
@@ -11,3 +11,18 @@ $(document).ready(function() {
       $('#myInput').trigger('focus')
   });
 });
+// getting values for pizza
+function getValue() {
+  var getPizza = document.getElementById("pizza");
+  var pizza =getPizza.options[getPizza.selectedIndex].value;
+  var getSize = document.getElementById("size");
+  var size = getSize.options[getSize.selectedIndex].value;
+  var getCrust = document.getElementById("crust");
+  var crust = getCrust.options[getCrust.selectedIndex].value;
+  var getToppings = document.getElementById("delivery");
+  var toppings = getToppings.options[getToppings.selectedIndex].value;
+  var getAmount = document.getElementById("amount").value;
+  var getTopping = document.getElementById("topping");
+  var topping = getTopping.options[getTopping.selectedIndex].value;
+  var getDelivery = document.getElementById("delivery");
+  var delivery = getDelivery.options[getDelivery.selectedIndex].value;
